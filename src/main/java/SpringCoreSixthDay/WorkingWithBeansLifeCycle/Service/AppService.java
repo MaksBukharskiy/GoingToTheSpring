76cb70b6878a58eb1e2e9847622d72919f86262c
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-08-03 13:49:48 +0500

    feat: Implemented bean lifecycle management using @PostConstruct, @PreDestroy and @Bean methods. Added database service with init/destroy callbacks.

---
+++ src/main/java/SpringCoreSixthDay/WorkingWithBeansLifeCycle/Service/AppService.java
@@ -0,0 +1,36 @@
+package SpringCoreSixthDay.WorkingWithBeansLifeCycle.Service;
+
+import SpringCoreSixthDay.WorkingWithBeansLifeCycle.Repository.DataBase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Service
+public class AppService{
+    private final DataBase dataBase;
+
+    @Autowired
+    public AppService(DataBase dataBase){
+        this.dataBase = dataBase;
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("🟢 Сервис инициализирован");
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        System.out.println("🔴 Сервис завершает работу");
+    }
+
+    public void processData() {
+        dataBase.insertData();
+        System.out.println("🔄 Данные обработаны");
+    }
+
+
+}
