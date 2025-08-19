fa090a69774a06bb52ee731d745090bd50d47da6
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-08-19 22:07:08 +0500

    feat: decided to start repeat Soring Data JPA mini program

--- src/main/java/SpringBootFifteenthDay/SpringDataGPAPracticing/Repository/UsersRepository.java
+++ src/main/java/SpringBootFifteenthDay/SpringDataGPAPracticing/Repository/UsersRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface UsersRepository extends JpaRepository <UserDatabase, Long>{
+public interface UsersRepository extends JpaRepository <UserDatabase, Long> {
     List<UserDatabase> findByUsername(String username);
+
 }
