e1fa5e34ea3bb67b3720b6472fc6e67763e0c3f3
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-08-20 23:43:43 +0500

    feat: Fix JPA entity: change id from long Id to Long id for proper Hibernate mapping.

--- src/main/java/SpringBootFifteenthDay/SpringDataGPAPracticing/Repository/UsersRepository.java
+++ src/main/java/SpringBootFifteenthDay/SpringDataGPAPracticing/Repository/UsersRepository.java
@@ -2,10 +2,9 @@ package SpringBootFifteenthDay.SpringDataGPAPracticing.Repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface UsersRepository extends JpaRepository <UserDatabase, Long> {
     List<UserDatabase> findByUsername(String username);
-
 }
+
