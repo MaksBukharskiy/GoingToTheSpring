c29c24c662fb3d481250f14d4f3b61dc19934a20
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-08-22 21:40:32 +0500

    feat: Add findByPassword(Integer) returning Optional for safe user lookup.

--- src/main/java/SpringBootFifteenthDay/SpringDataGPAPracticing/Repository/UsersRepository.java
+++ src/main/java/SpringBootFifteenthDay/SpringDataGPAPracticing/Repository/UsersRepository.java
@@ -1,10 +1,11 @@
 package SpringBootFifteenthDay.SpringDataGPAPracticing.Repository;
 
 
+import org.springframework.boot.autoconfigure.context.LifecycleAutoConfiguration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UsersRepository extends JpaRepository <UserDatabase, Long> {
-    List<UserDatabase> findByUsername(String username);
+    List<UserDatabase> findByPassword(Integer password);
 }
 
