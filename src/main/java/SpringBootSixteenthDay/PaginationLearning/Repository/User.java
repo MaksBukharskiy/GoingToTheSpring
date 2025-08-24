29bbfc003ea898c0729ef0ddcafdec50c0d9cbd5
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-08-25 01:45:48 +0500

    feat: Define JPA entity User with id, username, and age for database persistence.

---
+++ src/main/java/SpringBootSixteenthDay/PaginationLearning/Repository/User.java
@@ -0,0 +1,54 @@
+package SpringBootSixteenthDay.PaginationLearning.Repository;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+    private int age;
+
+    public User() {}
+
+    public User(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                 "id=" + id +
+                ", username=" + username +
+                ", age=" + age +
+                '}';
+    }
+
+}
