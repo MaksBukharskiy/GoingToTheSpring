0ffe6921e22552b05c641bae67ef2e1472f67a1a
freedom_lovver <maksbuharskij16@gmail.com>
2025-12-16 17:18:31 +0500

    add task conditions

--- src/main/java/TwoSums/TwoSum/Main.java
+++ src/main/java/TwoSums/TwoSum/Main.java
@@ -7,7 +7,11 @@ public class Main {
     public static int[] twoSum(int[] nums, int target) {
 
 
-        
+//      Given an array of integers nums and an integer target,
+//      return indices of the two numbers such that they add up to target.
+//      You may assume that each input has exactly one solution,
+//      and you may not use the same element twice.
+//      You can return the answer in any order.
 
         Map<Integer, Integer> map = new HashMap<>();
 
